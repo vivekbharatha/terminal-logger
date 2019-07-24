4f816792541d90d3b41ce853059fadd44c7555c0
vivekbharatha <vivek.bharatha@gmail.com>
2019-07-24 16:05:33 +0530

    adds minimal table view

--- db.js
+++ db.js
@@ -3,23 +3,29 @@ const db = new sqlite3('./database.db', {});
 
 let _module = {};
 
+let logCreateStmt;
+let logLatestStmt;
+
 // init
 _module.init = () => {
-    return db.prepare(`
+    db.prepare(`
     CREATE TABLE IF NOT EXISTS logs (
         date datetime PRIMARY KEY,
-        log text NOT NULL
+        log text NOT NULL,
+        tz_offset int NOT NULL
     )
     `).run();
+    logCreateStmt = db.prepare('INSERT INTO logs (date, log, tz_offset) VALUES (?, ?, ?)');
+    logLatestStmt = db.prepare('SELECT * FROM logs ORDER BY date DESC LIMIT ?');
 };
 
-const logCreateStmt = db.prepare('INSERT INTO logs (date, log) VALUES (?, ?)');
-const logLatestStmt = db.prepare('SELECT * FROM logs ORDER BY date DESC LIMIT ?');
+
 
 let logs = {};
 
 logs.create = (data) => {
-    return logCreateStmt.run(data.date, data.log);
+
+    return logCreateStmt.run(data.date.toISOString(), data.log, data.date.getTimezoneOffset());
 };
 
 logs.getLatest = (numberOfRecords) => {
